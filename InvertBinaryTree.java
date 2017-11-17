80a6e8c31e0516c9f045f33d0799375b897fca18
Shantanu <shantanu.sengupta2008@gmail.com>
2017-11-17 14:03:20 -0500

    Invert Binary Tree

---
+++ InvertBinaryTree.java
@@ -0,0 +1,22 @@
+
+public class InvertBinaryTree {
+	
+	static public TreeNode invertTree(TreeNode root) {
+        if(root==null) {
+        	return null;
+        }
+        TreeNode leftTree = invertTree(root.left);
+        TreeNode rightTree = invertTree(root.right);
+        
+        root.left = rightTree;
+        root.right = leftTree;
+        
+		return null;
+    }
+
+	public static void main(String[] args) {
+		 
+
+	}
+
+}
