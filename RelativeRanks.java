24ce034f0d6e21f72f82940973cbae3d043c3b4e
Shantanu <shantanu.sengupta2008@gmail.com>
2018-04-05 08:03:22 -0400

    Leetcode

---
+++ RelativeRanks.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class RelativeRanks {
+	
+	static public String[] findRelativeRanks(int[] nums) {
+		
+		HashMap<Integer, String> map = new HashMap<Integer, String>();
+        int[] copy = nums.clone();
+        Arrays.sort(nums);
+        
+        int rank = 4;
+        for (int i = nums.length-4; i >= 0; i--) {
+			map.put(nums[i], Integer.toString(rank++));
+		}
+        map.put(nums[nums.length-1], "Gold Medal");
+        if(nums.length >= 2)
+        	map.put(nums[nums.length-2], "Silver Medal");
+        if(nums.length >= 3)
+        map.put(nums[nums.length-3], "Bronze Medal");
+        
+        String[] result = new String[nums.length];
+        for (int i = 0; i < copy.length; i++) {
+			result[i] = map.get(copy[i]);
+		}
+        return result;
+        
+    }
+
+	public static void main(String[] args) {
+		int[] nums = {4};
+		System.out.println(Arrays.toString(findRelativeRanks(nums)));
+	}
+
+}
