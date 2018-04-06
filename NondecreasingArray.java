9f3a17908cb0592c27e5203d625049b3b2cac5bc
Shantanu <shantanu.sengupta2008@gmail.com>
2018-04-05 22:22:41 -0400

    Leetcode

---
+++ NondecreasingArray.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class NondecreasingArray {
+	
+	static public boolean checkPossibility(int[] nums) {
+		return true;
+    }
+
+	public static void main(String[] args) {
+		int[] nums = {4,4,3};
+		System.out.println(Arrays.toString(nums)+"\t"+checkPossibility(nums));
+
+	}
+}
--- NumberofBoomerangs.java
+++ NumberofBoomerangs.java
@@ -1,11 +1,40 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class NumberofBoomerangs {
 	
+	public static int getDifference(int[] a,int[] b) {
+		return (a[0]-b[0])*(a[0]-b[0]) + (a[1]-b[1])*(a[1]-b[1]);
+	}
+	
+	
+	
 	public static int numberOfBoomerangs(int[][] points) {
-        
+		
+		int result = 0;
+		for (int i = 0; i < points.length; i++) {
+			HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+			
+			for (int j = 0; j < points.length; j++) {
+				if(i!=j) {
+					int x = getDifference(points[i], points[j]);
+					map.put(x, map.getOrDefault(x, 0)+1);
+				}
+			}
+			for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+				if(entry.getValue() > 1) {
+					result += (entry.getValue()*(entry.getValue()-1));
+				}
+			}
+		}
+		
+		return result;
     }
 
 	public static void main(String[] args) {
-
+		int[][] points = {{0,0},{1,0},{2,0},{3,0},{0,0}};
+//		int[][] points = {{0,0},{1,0},{-1,0},{0,1},{0,-1}};
+		System.out.println(numberOfBoomerangs(points));
 
 	}
 
